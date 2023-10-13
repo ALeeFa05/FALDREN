3924910b8409fa796079dfea9796e8c7df9e2ca4
ALeeFa05 <l23211955@tectijuana.edu.mx>
2023-10-13 14:36:38 -0700

    Everything should work

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+
+
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 const activeChainId = ChainId.SolanaDevnet;
--- pages/estudiante.js
+++ pages/estudiante.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import styles from '../styles/estudiante.css';
+
+
+
 export default function Demo() {
     return (
         <div>
-            <h1 className={styles.heading}>¡Bienvenido Estudiante!</h1>
-            <form className={styles.form}>
+            <h1 >¡Bienvenido Estudiante!</h1>
+            <form >
                 <h2>Favor de ingresar su no. de control:</h2>
-                <label className={styles.label}>No. de Control*</label>
+                <label >No. de Control*</label>
                 <input type="text" name="txtnoControl" id="txtnoControl" required />
                 <br />
                 <input type="submit" name="btnEntrar" id="btnEntrar" value="Ingresar" />
