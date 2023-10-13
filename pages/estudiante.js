ba65b6eb39bb4de4bda9633b0627a22543cedba2
ALeeFa05 <l23211955@tectijuana.edu.mx>
2023-10-13 14:23:57 -0700

    estudiante css bug fixed

--- pages/estudiante.js
+++ pages/estudiante.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/estudiante.module.css';
+import styles from '../styles/estudiante.css';
 export default function Demo() {
     return (
         <div>
